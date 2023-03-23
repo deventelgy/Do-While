cb773f55c089f0e65b720bc496cd839d5795b188
Alin Piero Javier Cruz <alin.javier@entelgy.pe.local>
2023-03-23 16:31:57 -0500

    uso de do while

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int val =80;
+        do{
+            System.out.println(val);
+            val *=2;
+            System.out.println(val);
+        }while(val<25);
+    }
+}
